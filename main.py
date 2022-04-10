5ee38483b1b4dbb401da5bbae27475e798704739
DenisSK228 <9293817575den@gmail.com>
2022-04-10 14:11:26 +0700

    window

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import tkinter as tk
+from tkinter import messagebox
 
 window = tk.Tk()
 window.geometry("480x640")
 window.title("Password Manager")
 
+login = tk.StringVar()
+password = tk.StringVar()
+
 login = tk.Entry()
 login.place(relx=.5, rely=.1, anchor="c")
 
+password = tk.Entry(show='*')
+password.place(relx=.5, rely=.2, anchor="c")
+
+submit_button = tk.Button(text="Submit", command=lambda: print(login.get(), password.get()))
+submit_button.place(relx=.5, rely=.5, anchor="c")
 
 window.mainloop()
