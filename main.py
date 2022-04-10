fc085ae7215473235eb6c82b83e155e98c55c312
DenisSK228 <9293817575den@gmail.com>
2022-04-10 14:00:04 +0700

    1

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import tkinter as tk
 
 window = tk.Tk()
+window.geometry("480x640")
+window.title("Password Manager")
+
+login = tk.Entry()
+login.place(relx=.5, rely=.1, anchor="c")
+
+
 window.mainloop()
