e22deacfb7fb0bfcbb91971040e4cf5d269d14a6
DenisSK228 <9293817575den@gmail.com>
2022-04-17 16:18:52 +0700

    17.04

--- query.py
+++ query.py
@@ -4,7 +4,7 @@ def insert(data):
     try:
         sqlite_connection = sqlite3.connect('testdb.db')
         cursor = sqlite_connection.cursor()
-        print("Подключен к БД")
+        print("INS: Подключен к БД")
         sqlite_insert_query = data
         count = cursor.execute(sqlite_insert_query)
         sqlite_connection.commit()
@@ -12,8 +12,26 @@ def insert(data):
         cursor.close()
 
     except sqlite3.Error as error:
-        print("Ошибка при работе с БД", error)
+        print("INS: Ошибка при работе с БД", error)
     finally:
         if sqlite_connection:
             sqlite_connection.close()
-            print("Соединение с БД закрыто")
+            print("INS: Соединение с БД закрыто")
+
+def out():
+    try:
+        sqlite_connection = sqlite3.connect('testdb.db')
+        cursor = sqlite_connection.cursor()
+        print("OUT: Подключен к БД")
+        sqlite_select_query = """SELECT * from logpw"""
+        cursor.execute(sqlite_select_query)
+        records = cursor.fetchall()
+        cursor.close()
+        return records
+
+    except sqlite3.Error as error:
+        print("OUT: Ошибка при работе с БД", error)
+    finally:
+        if sqlite_connection:
+            sqlite_connection.close()
+            print("OUT: Соединение с БД закрыто")
