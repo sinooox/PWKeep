f4f546497d3b572d0736bc2503dc8be73b03514e
sinooox <dima.i.fedulov@gmail.com>
2022-04-24 14:59:17 +0700

    reg

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 import query as q
+import os
 
 window = tk.Tk()
 window.geometry("400x500")
@@ -10,6 +11,11 @@ tabControl = ttk.Notebook(window)
   
 tab1 = ttk.Frame(tabControl)
 tab2 = ttk.Frame(tabControl)
+reg = ttk.Frame(tabControl)
+
+def update():
+    window.destroy()
+    os.system('main.py')
 
 l = ttk.Label(tab2, text='ID Name Login Password')
 l.pack(anchor='w')
@@ -25,23 +31,39 @@ for i in lst:
 listbox.pack(expand=True, fill="both")
 scrollbar.config(command=listbox.yview)
 
-refresh_button = ttk.Button(tab2, text="Refresh", command=tab2.update())
+refresh_button = ttk.Button(tab2, text="Refresh", command=lambda: update())
 refresh_button.pack(anchor='s')
 
+ttk.Label(tab1, text="Enter Name").grid(row=0, column=0, sticky='W', pady=35)
+ttk.Label(tab1, text="Enter Login").grid(row=1, column=0, sticky='W', pady=0)
+ttk.Label(tab1, text="Enter Password").grid(row=2, column=0, sticky='W', pady=25)
+
+tabControl.add(reg, text='Register')
 tabControl.add(tab1, text='Enter')
 tabControl.add(tab2, text='Saved')
 tabControl.pack(expand=1, fill="both")
+
+reg_login = tk.StringVar()
+reg_password = tk.StringVar()
+master_password = tk.StringVar()
+
+reg_login = ttk.Entry(reg)
+reg_login.place(relx=.5, rely=.1, anchor="c")
+
+reg_password = ttk.Entry(reg, show='*')
+reg_password.place(relx=.5, rely=.2, anchor="c")
+
+master_password = ttk.Entry(reg, show='*')
+master_password.place(relx=.5, rely=.3, anchor="c")
 
 name = tk.StringVar()
 login = tk.StringVar()
 password = tk.StringVar()
 
 name = ttk.Entry(tab1)
-name.insert(0, "Name")
 name.place(relx=.5, rely=.1, anchor="c")
 
 login = ttk.Entry(tab1)
-login.insert(0, "Login")
 login.place(relx=.5, rely=.2, anchor="c")
 
 password = ttk.Entry(tab1, show='*')
@@ -52,8 +74,17 @@ def id():
     new_id = int(prev_id[0]) + 1
     return new_id
 
+def reg_id():
+    prev_id = [q.reg_out()][-1][-1]
+    new_id = int(prev_id[0]) + 1
+    return new_id
+
 submit_button = ttk.Button(tab1, text="Submit", 
     command=lambda:q.insert(f"""INSERT INTO logpw (id, name, login, password) VALUES ('{id()}', '{name.get()}', '{login.get()}', '{password.get()}');"""))
+submit_button.place(relx=.5, rely=.5, anchor="c")
+
+submit_button = ttk.Button(reg, text="Submit", 
+    command=lambda:q.insert(f"""INSERT INTO registred (id, login, password, masterpassword) VALUES ('{reg_id()}', '{reg_login.get()}', '{reg_password.get()}', '{master_password.get()}');"""))
 submit_button.place(relx=.5, rely=.5, anchor="c")
 
 window.mainloop()
--- query.py
+++ query.py
@@ -35,3 +35,21 @@ def out():
         if sqlite_connection:
             sqlite_connection.close()
             print("OUT: Соединение с БД закрыто")
+
+def reg_out():
+    try:
+        sqlite_connection = sqlite3.connect('testdb.db')
+        cursor = sqlite_connection.cursor()
+        print("OUT: Подключен к БД")
+        sqlite_select_query = """SELECT * from registred"""
+        cursor.execute(sqlite_select_query)
+        records = cursor.fetchall()
+        cursor.close()
+        return records
+
+    except sqlite3.Error as error:
+        print("OUT: Ошибка при работе с БД", error)
+    finally:
+        if sqlite_connection:
+            sqlite_connection.close()
+            print("OUT: Соединение с БД закрыто")
